287f08dc990c07664ff268c3d941ff6f25905d99
이진희 <80159575+gyulhongsy@users.noreply.github.com>
2026-05-19 17:47:23 +0900

    260519

---
+++ src/main/java/study/week05/Solution_258705_산모양타일링.java
@@ -0,0 +1,14 @@
+package study.week05;
+
+public class Solution_258705_산모양타일링 {
+	
+	static final int MOD = 10007;
+	
+	public int solution(int n, int[] tops) {
+        int answer = 0;
+        
+        
+        
+        return answer;
+    }
+}
