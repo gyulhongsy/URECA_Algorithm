ac143d68bb985051eddb758d4f6bb8492905ea80
이진희 <80159575+gyulhongsy@users.noreply.github.com>
2026-06-09 17:45:13 +0900

    260609

--- src/main/java/study/week08/Solution_142085_디펜스게임.java
+++ src/main/java/study/week08/Solution_142085_디펜스게임.java
@@ -1,8 +1,31 @@
 package study.week08;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class Solution_142085_디펜스게임 {
     public int solution(int n, int k, int[] enemy) {
         int answer = 0;
+        int size = enemy.length;
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < size; i++) {
+            n -= enemy[i];
+            queue.add(enemy[i]);
+            answer++;
+
+            if (n < 0) {
+                if (k > 0) {
+                    n += queue.poll();
+                    k--;
+                } else {
+                    answer--;
+                    break;
+                }
+            } //if - 병사 수 마이너스
+        } //for
+
         return answer;
     } //solution
 }
---
+++ src/main/java/study/week08/Solution_42842_카펫.java
@@ -0,0 +1,8 @@
+package study.week08;
+
+public class Solution_42842_카펫 {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = {};
+        return answer;
+    }
+}
